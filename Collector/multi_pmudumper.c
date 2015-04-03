2129f45da36ea2e994dd00ce68de827a13f83950
Gaurav <ga286@cornell.edu>
2015-04-03 02:44:34 -0400

    CLean up. Removing global variables.
    Signed-off-by: Gaurav <ga286@cornell.edu>

--- Collector/multi_pmudumper.c
+++ Collector/multi_pmudumper.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <string.h>
+#include <math.h>
 #include <netinet/in.h>
 #include "c37.h"
 
@@ -17,6 +18,8 @@
 #define BUF_SIZE 4096
 #define MAX_BUS 100
 #define PHASE_ANGLE_THRESHOLD		0.5
+
+const double EPS = 1e-20;
 
 #ifdef DEBUG_ENABLE
 #define DUMP_PACKET(pkt) do { \
@@ -43,7 +46,7 @@
 
 /* An entry maintained per data received from DC */
 typedef struct dc_data_entry {
-	uint32_t	soc; 			/* Time Stamp in c37 terminology */
+	uint32_t	soc; 				/* Time Stamp in c37 terminology */
 	float		voltage_amplitude; 	/* Reported to OC */
 	float 		voltage_angle;		/* Reported to OC */
 } dc_data_entry_t;
@@ -52,16 +55,28 @@ typedef struct dc_data_entry {
 typedef struct data_collector_thread_data {
 	int 	port;		/* Port on which this thread is listening */
 	int 	thread_id;	/* Id */
-	int	cur_index;	/* Current index in the below array */
+	int		cur_index;	/* Current index in the below array */
 	dc_data_entry_t 	received_data[MAX_HISTORY]; /* data from DC */
 } data_collector_thread_data_t;
 
 #define data_collector_thread_data_s (sizeof(data_collector_thread_data_t))
 
+/* A structure representing details of a bus */
+typedef struct bus_details {
+	int 	bus_id; 				/* ID of the bus */
+	char*	bus_name;				/* Name of the bus */
+	double	bus_base_kilo_voltage;	/* Base kilo voltage of bus */
+	int		bus_type;				/* Type of bus */
+	int		area_number; 			/* Area number */
+	char*	bus_info_serialized;	/* All the above fields in comma separated format */
+	int* 	connected_buses;		/* Buses which are connected to this bus */
+} bus_details_t;
+
 /* A structure passed to data-supplier thread */
 typedef struct data_supplier_thread_data {
-	int 	port;		/* Port on which this Data Supplier thread is listening */
-	int	max_dc_srv_thread; /* Max Data Collector thread */
+	int 			port;				/* Port on which this Data Supplier thread is listening */
+	int				num_dc_svr_thread; 	/* Number of Data Collector threads = number of buses */
+	bus_details_t*	buses_information;	/* An array of all bus information */
 	data_collector_thread_data_t **data_collector;
 } data_supplier_thread_data_t;
 
@@ -70,9 +85,9 @@ typedef struct data_supplier_thread_data {
  */
 struct prog_args {
 	char *name;
-	char *dc_start_port;
-	char *dc_instance;
-	char *ds_port;
+	char *dc_start_port;		/* Starting port for data collector threads */
+	char *num_dc_svr_threads;	/* Number of data collector threads */
+	char *ds_port;				/* Data supplier port */
 } prog_args;
 
 static void usage(){
@@ -84,75 +99,86 @@ static void usage(){
 	exit(1);
 }
 
-// Make it dynamically
-struct busDetails {
-	char *busdetails_info;
-	float voltage_amplitude;
-	float voltage_angle;
-	int *busMap;
-};
-
-struct busDetails bus[MAX_BUS];
-
-void init_bus_details() {
+/* Reads and parses busdetails.txt and busmap.txt and populates the given array of bus details */
+static void init_bus_details(bus_details_t* bus_details_arr, int num_buses) {
 	FILE *fp = NULL;
 	char *line = NULL;
 	char *token = NULL;
-	size_t len = 0;
-    int busdata_len = 0;
+	size_t len = 0, data_len = 0;
 	ssize_t read = 0;
-	char *bus_id = NULL, *map_id = NULL, *bus_map = NULL;
-	int id = 0, m_id = 0, i = 0, j = 0;
-
-	// char *token = NULL;
+	int cur_bus = 0, connected_bus_index = 0;
+	
+	/* Parse busdetails.txt */
 	fp = fopen(BUSCONFIG_FILENAME, "r");
 	while ((read = getline(&line, &len, fp)) != -1) {
+		
+		/* Strip newline character from end */
 		token = strtok(line,"\n");
-                busdata_len = strlen(token);
-                bus[i].busdetails_info = (char*) malloc((busdata_len+1)*sizeof(char));
-                strncpy(bus[i].busdetails_info,token,busdata_len);
-                bus[i].busdetails_info[busdata_len] = '\0';
-		//bus[i].busdetails_info = bus_info;
-				bus[i].busMap = NULL;
-                DEBUG_MSG("%s", bus[i].busdetails_info);
-                i++;
+		
+		/* Fill serialized data */
+		data_len = strlen(token);
+		bus_details_arr[cur_bus].bus_info_serialized = (char*) malloc((data_len+1)*sizeof(char));
+		strncpy(bus_details_arr[cur_bus].bus_info_serialized, token, data_len);
+		bus_details_arr[cur_bus].bus_info_serialized[data_len] = '\0';
+				
+		/* Also deserialize it */
+		/* get bus id */
+		token = strtok(token, ",");
+		sscanf(token, "%d", &bus_details_arr[cur_bus].bus_id);
+		
+		/* get bus name */
+		token = strtok(NULL, ",");
+		data_len = strlen(token);
+		bus_details_arr[cur_bus].bus_name = (char*) malloc((data_len+1)*sizeof(char));
+		strncpy(bus_details_arr[cur_bus].bus_name, token, data_len);
+		bus_details_arr[cur_bus].bus_name[data_len] = '\0';
+		
+		/* get base kilo voltage */
+		token = strtok(NULL, ",");
+		sscanf(token, "%lf", &bus_details_arr[cur_bus].bus_base_kilo_voltage);
+		
+		/* get bus type */
+		token = strtok(NULL, ",");
+		sscanf(token, "%d", &bus_details_arr[cur_bus].bus_type);
+		
+		/* get area number */
+		token = strtok(NULL, ",");
+		sscanf(token, "%d", &bus_details_arr[cur_bus].area_number);
+		
+		bus_details_arr[cur_bus].connected_buses = NULL;
+		cur_bus++;
+		
+		/* avoid overflow */
+		if(cur_bus >= num_buses) {
+			break;
+		}
 	}
-
 	fclose(fp);
 
+	/* Parse busmap.txt */
 	fp = fopen(BUSMAP_FILENAME, "r");
 	while ((read = getline(&line, &len, fp)) != -1) {
-		id  = 0;
-		i = 0;
-		bus_id = strtok(line, ":");
-		while (bus_id[i] != '\0') {
-			id = id * 10 + bus_id[i] - '0';
-			i++;
+		
+		/* get current bus */
+		token = strtok(line, ":");
+		sscanf(token, "%d", &cur_bus);
+		
+		/* get connected buses */
+		token = strtok(NULL, ":");
+		if(token != NULL) {
+			bus_details_arr[cur_bus].connected_buses = (int *) malloc(sizeof(int) * (strlen(token)/2 + 1));
 		}
-		bus_map = strtok(NULL, ":");
-		if (bus_map != NULL)
-			bus[id].busMap = (int *) malloc(sizeof(int) * (strlen(bus_map)/2 + 1));
-
-		j = 0;
-		map_id = strtok(bus_map, ",");
-		while (map_id!= NULL) {
-			m_id = 0;
-			i = 0;
-			while (map_id[i] != '\0') {
-				m_id = m_id * 10 + map_id[i] - '0';
-				i++;
-			}
-			bus[id].busMap[j++] = m_id;
-			map_id = strtok(NULL, ",");
+		
+		connected_bus_index = 0;
+		token = strtok(token, ",");
+		while (token!= NULL) {
+			sscanf(token, "%d", &bus_details_arr[cur_bus].connected_buses[connected_bus_index]);
+			connected_bus_index++;
+			token = strtok(NULL, ",");
 		}
-		bus[id].busMap[j] = -1;
+		bus_details_arr[cur_bus].connected_buses[connected_bus_index] = -1;
 	}
 }
-
-char *get_bus_details(int i){
-	return bus[i].busdetails_info;
-}
-
 
 static int do_copy(int fd, data_collector_thread_data_t* dc_thread_data){
 	FILE *input = fdopen(fd, "r");
@@ -195,11 +221,11 @@ static int do_copy(int fd, data_collector_thread_data_t* dc_thread_data){
 		dc_thread_data->cur_index = (dc_thread_data->cur_index + 1) % MAX_HISTORY;
 
 		/* Write the packet to standard output */
-//		write_c37_packet_readable(stdout, pkt);
+		//write_c37_packet_readable(stdout, pkt);
 		free(pkt);
 	}
 
-			fprintf(stderr, "%s: do_copy: close fd\n", prog_args.name);
+	fprintf(stderr, "%s: do_copy: close fd\n", prog_args.name);
 	fclose(input);
 
 	return 1;
@@ -243,14 +269,14 @@ static void get_args(int argc, char *argv[]){
 				}
 				break;
 			case 'i':
-				if (prog_args.dc_instance != 0) {
+				if (prog_args.num_dc_svr_threads != 0) {
 					fprintf(stderr, "%s: can specify number of DC instance\n",
 					                prog_args.name);
 					exit(1);
 				}
-				if ((prog_args.dc_instance = optarg) == 0) {
+				if ((prog_args.num_dc_svr_threads = optarg) == 0) {
 					fprintf(stderr, "%s: -i takes number of DC instance\n",
-					        prog_args.dc_instance);
+					        prog_args.num_dc_svr_threads);
 					exit(1);
 				}
 				break;
@@ -282,11 +308,12 @@ static void get_args(int argc, char *argv[]){
 static void do_supply(int s, data_supplier_thread_data_t* data_supplier_thread_data)
 {
 	int fd, id, read_index;
-	int32_t num_instances = data_supplier_thread_data->max_dc_srv_thread;
+	int32_t num_dc_threads = data_supplier_thread_data->num_dc_svr_thread;
 	data_collector_thread_data_t **dc_thread_data_array = data_supplier_thread_data->data_collector;
+	bus_details_t* buses_information = data_supplier_thread_data->buses_information;
 	char affected[BUF_SIZE];
-	int i = 0, j = 0;
-	float diff = 0.0;
+	int connected_bus_index = 0, connected_bus = 0;
+	float voltage_angle_diff = 0.0f, voltage_amplitude[MAX_BUS], voltage_angle[MAX_BUS];
 
 	for (; ;) {
 		printf("Waiting for connection...\n");
@@ -312,58 +339,55 @@ static void do_supply(int s, data_supplier_thread_data_t* data_supplier_thread_d
 		}
 		DEBUG_MSG("Input Command: %s", input);
 		*/
-		char output[BUF_SIZE*MAX_BUS]; // MACROSIZE* BUSNO;
+		char output[BUF_SIZE*MAX_BUS]; 
 		memset(&output,0,BUF_SIZE*MAX_BUS);
-		//"AFFECTEDLIST":
+		
 		char end_char ='_';
-		for (id = 0; id < num_instances; id++) {
-			if (id == (num_instances-1)){
+		for (id = 0; id < num_dc_threads; id++) {
+			if (id == (num_dc_threads-1)){
 				end_char=':';
 			}
 			char busdata[BUF_SIZE];
 			memset(&busdata,0,BUF_SIZE);
+			
 			/* TODO: Lock for synchronizing read and writes in data_collector_thread_data_t */
 			read_index 	= dc_thread_data_array[id]->cur_index;
-			bus[id].voltage_amplitude = dc_thread_data_array[id]->received_data[read_index].voltage_amplitude;
-			bus[id].voltage_angle = dc_thread_data_array[id]->received_data[read_index].voltage_angle;
-			sprintf(busdata,"%s,%f,%f%c",get_bus_details(id),
-				bus[id].voltage_amplitude, bus[id].voltage_angle, end_char);
+			
+			voltage_amplitude[id] = dc_thread_data_array[id]->received_data[read_index].voltage_amplitude;
+			voltage_angle[id] = dc_thread_data_array[id]->received_data[read_index].voltage_angle;
+			
+			sprintf(busdata,"%s,%f,%f%c", buses_information[id].bus_info_serialized,
+										  voltage_amplitude[id], voltage_angle[id], end_char);
 			
 			strcat(output,busdata);
 			DEBUG_MSG("%s",busdata);
 		}
 		
 		end_char = ':';
-		for (id = 0; id < num_instances; id++) {
-			i = 0;
-			if (bus[id].busMap != NULL) {
-				DEBUG_MSG("\n\nin bus map\n\n");
+		for (id = 0; id < num_dc_threads; id++) {
+			if (buses_information[id].connected_buses != NULL) {
 			
-			while ((j = bus[id].busMap[i++]) != -1) {
-				if (bus[id].voltage_angle > bus[j].voltage_angle)
-					diff = bus[id].voltage_angle - bus[j].voltage_angle;
-				else
-					diff = bus[j].voltage_angle - bus[id].voltage_angle;
-				if (diff > PHASE_ANGLE_THRESHOLD) {
-					strcpy(affected, "");
-					if (end_char == ':') {
-						sprintf(affected,"%d,%d",id, j);
-						end_char ='_';
-					}
-					else
-						sprintf(affected,"%c%d,%d", end_char, id, j);
-					strcat(output,affected);
-					DEBUG_MSG("%s %f",affected, diff);
+				for(connected_bus_index = 0; buses_information[id].connected_buses[connected_bus_index] != -1; ++connected_bus_index) {
+					connected_bus = buses_information[id].connected_buses[connected_bus_index];
+					
+					voltage_angle_diff = fabs(voltage_angle[id] - voltage_angle[connected_bus]);
+					
+					if(voltage_angle_diff - PHASE_ANGLE_THRESHOLD > EPS) {
+						strcpy(affected, "");
+						if (end_char == ':') {
+							sprintf(affected,"%d,%d",id, connected_bus);
+							end_char ='_';
+						} else {
+							sprintf(affected,"%c%d,%d", end_char, id, connected_bus);
+						}
+						strcat(output,affected);
+					} 
 				}
-				DEBUG_MSG("\n DIFF %f\n", diff);
-				diff = 0.0;
-			}
 			}
 		}
-			
 		
         strcat(output,"\r\n");
-		DEBUG_MSG("OUTPUT:->%s",output);
+		DEBUG_MSG("Data being sent to Operator Console: %s",output);
 		send(fd, &output, strlen(output),0);
 		close(fd);
 		printf("Connection closed...\n");
@@ -456,11 +480,9 @@ int main(int argc, char *argv[]){
 	/* Get and parse arguments */
 	get_args(argc, argv);
 
-	init_bus_details();
-
 	int dc_start_port = DFL_DC_PORT;
 	int ds_port = DFL_DS_PORT;
-	int dc_instance = MAX_DC_SERVER_THREADS;
+	int num_dc_svr_threads = MAX_DC_SERVER_THREADS;
 
 	/* DC start port must be positive */
 	if (prog_args.dc_start_port != 0) {
@@ -479,26 +501,30 @@ int main(int argc, char *argv[]){
 		}
 	}
 	/* DC number of instance must be positive */
-	if (prog_args.dc_instance != 0) {
-		if ((dc_instance = atoi(prog_args.dc_instance)) <= 0) {
+	if (prog_args.num_dc_svr_threads != 0) {
+		if ((num_dc_svr_threads = atoi(prog_args.num_dc_svr_threads)) <= 0) {
 			fprintf(stderr, "%s: DC instance must be positive integer\n",
 			        prog_args.name);
 			exit(1);
 		}
 	}
 	
-	data_collector_thread_ids = (pthread_t*) malloc(sizeof(pthread_t)*dc_instance);
+	/* Array to hold thread ids of all data collector threads */
+	data_collector_thread_ids = (pthread_t*) malloc(sizeof(pthread_t)*num_dc_svr_threads);
 	if (NULL == data_collector_thread_ids) {
 		fprintf(stderr, "%s: Unable to allocate memory \n", prog_args.name);
 		exit(1);
 	}
-	dc_thread_data_array = (data_collector_thread_data_t **)malloc(sizeof(data_collector_thread_data_t *)*dc_instance);
+	
+	/* Array to hold all data objects passed to data collector threads */
+	dc_thread_data_array = (data_collector_thread_data_t **)malloc(sizeof(data_collector_thread_data_t *)*num_dc_svr_threads);
 	if (NULL == dc_thread_data_array ) {
 		fprintf(stderr, "%s: Unable to allocate memory \n", prog_args.name);
 		exit(1);
 	}
+	
 	/* Start Data Collector servers */
-	for (i=0; i< dc_instance; i++) {
+	for (i=0; i< num_dc_svr_threads; i++) {
 		dc_thread_data_array[i] = (data_collector_thread_data_t*)malloc(data_collector_thread_data_s);
 		dc_thread_data_array[i]->cur_index = 0;
 		dc_thread_data_array[i]->port = dc_start_port + i;
@@ -508,17 +534,23 @@ int main(int argc, char *argv[]){
 		pthread_create(	&data_collector_thread_ids[i], NULL,
 						data_collector_thread_fxn, (void*)dc_thread_data_array[i] );
 	}
-
+	
+	/* Fill up bus details */
+	bus_details_t* buses_information = (bus_details_t*)malloc(sizeof(bus_details_t) * num_dc_svr_threads);
+	init_bus_details(buses_information, num_dc_svr_threads);
+	
+	/* Start Data Supplier thread */
 	DEBUG_MSG("Creating Data Supplier Thread");
 	data_supplier_thread_data.data_collector = dc_thread_data_array;
-	data_supplier_thread_data.max_dc_srv_thread = dc_instance;
+	data_supplier_thread_data.num_dc_svr_thread = num_dc_svr_threads;
 	data_supplier_thread_data.port  = ds_port;
+	data_supplier_thread_data.buses_information = buses_information;
 	/* Data Supplier Thread */
 	pthread_create(&data_supplier_thread_id, NULL, data_supplier_thread_fxn, (void*)&data_supplier_thread_data);
 
 	/* Wait for Data Collector threads */
-	for(i=0; i< dc_instance; i++) {
-		//pthread_join(data_collector_thread_ids[i], NULL);
+	for(i=0; i< num_dc_svr_threads; i++) {
+		pthread_join(data_collector_thread_ids[i], NULL);
 	}
 	
 	/* Wait for Data Supplier thread */
